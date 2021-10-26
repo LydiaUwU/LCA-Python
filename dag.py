6235b822d6f5cf093a1fe69fdc774e167bc5a0e4
lydia <lydiamacbride@protonmail.com>
2021-10-26 21:55:40 +0100

    Created DAG branch

---
+++ dag.py
@@ -0,0 +1,14 @@
+# @Author: Lydia MacBride. 19333944
+#
+# Implementation of a Directed Acyclic Graph in Python
+#
+# I love my wives
+
+# DAG Class
+class DAG:
+    pass
+
+
+# Node class for DAG
+class Node:
+    pass
